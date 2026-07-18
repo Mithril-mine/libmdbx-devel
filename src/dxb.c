b37a79b8d998a1e20fb170366ea8cbed59c42dda
Леонид Юрьев (Leonid Yuriev) <leo@yuriev.ru>
2026-07-18 11:22:32 +0300

    mdbx: fix build with Valgrind/ASAN after `dfd307797ea3da385961041395dfb7e05aae59c6`.

--- src/dxb.c
+++ src/dxb.c
@@ -375,7 +375,7 @@ void dxb_sanitize_tail(MDBX_env *env, MDBX_txn *txn) {
     } else if ((env->flags & MDBX_NOSTICKYTHREADS) ? (env->basal_txn->owner != 0)
                                                    : (env->basal_txn->owner == osal_thread_self())) {
       /* inside write-txn */
-      last = meta_recent(env, &env->basal_txn->wr.troika).ptr_v->geometry.first_unallocated;
+      last = meta_recent(env, &env->basal_txn->tw.troika).ptr_v->geometry.first_unallocated;
     } else if (lck_txn_lock(env, true) == MDBX_SUCCESS) {
       /* no write-txn */
       last = NUM_METAS;
