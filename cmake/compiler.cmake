d1a892068dbae103e433375df20790f4aaf27a9d
Леонид Юрьев (Leonid Yuriev) <leo@yuriev.ru>
2026-08-05 03:10:00 +0300

    mdbx-cmake: add `/experimental:c11atomics` for bad MSVC compiler to enable C11 atomics (backport).

--- cmake/compiler.cmake
+++ cmake/compiler.cmake
@@ -431,6 +431,9 @@ if(CMAKE_C_COMPILER_LOADED)
       endif()
     endif()
   endif()
+  if(MSVC AND NOT CMAKE_COMPILER_IS_CLANG)
+    check_c_compiler_flag("/experimental:c11atomics" MSVC_C11_MAD_ATOMICS)
+  endif()
 endif()
 
 # Check for LTO support by GCC
