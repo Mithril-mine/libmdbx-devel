b25ed756c4a7db51a63d3f26269fc44872fa313b
Леонид Юрьев (Leonid Yuriev) <leo@yuriev.ru>
2026-08-11 13:53:01 +0300

    mdbx: prefer non-portable `fallocate()` to avoid emulation in glibc (backport).

--- src/osal.c
+++ src/osal.c
@@ -1633,7 +1633,10 @@ int osal_fsetsize(mdbx_filehandle_t fd, const uint64_t length) {
 
   const uint64_t allocated = UINT64_C(512) * info.st_blocks;
   if (length > allocated) {
-#if defined(__APPLE__)
+#if defined(FALLOC_FL_ALLOCATE_RANGE) && defined(FALLOC_FL_KEEP_SIZE)
+    /* prefer a low-level non-portable function to avoid glibc emulation if the file system does not support the operation. */
+    int err = fallocate(fd, FALLOC_FL_ALLOCATE_RANGE | FALLOC_FL_KEEP_SIZE, 0, length);
+#elif defined(__APPLE__)
     fstore_t store = {
         .fst_flags = F_ALLOCATECONTIG, .fst_posmode = F_PEOFPOSMODE, .fst_offset = 0, .fst_length = length};
     int err = MDBX_SUCCESS;
